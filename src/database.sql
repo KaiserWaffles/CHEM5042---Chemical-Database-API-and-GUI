8ae68c5222902c1a149b5be42b8df8d56b00cf74
KaiserWaffles <148008162+KaiserWaffles@users.noreply.github.com>
2026-02-18 14:41:12 +0000

    uploaded .sdf file and changed database.py into .sql

---
+++ src/database.sql
@@ -0,0 +1,15 @@
+--Database
+"""
+Question: How would a library of 1,000,000+ compounds affect database compared to a library of 100 instead; indexing the compound/molecule
+"""
+CREATE TABLE Compound(
+
+
+
+)
+
+--Do i need this or should i just make it into a single table
+CREATE TABLE Parameters(
+
+
+)
